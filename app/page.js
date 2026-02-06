2b4605c0a105262c3ff7231e39ffa7660f09b284
ajaymanath96 <ajaymanath96@gmail.com>
2026-02-06 12:47:09 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -4,8 +4,10 @@ import Map from "./components/Map/Map";
 export default function Home() {
   return (
     <div className="flex h-screen overflow-hidden bg-gray-100 dark:bg-black">
-      {/* Sidebar - Left Side */}
-      <Sidebar activeItem="jobs-near-you" />
+      {/* Sidebar - Left Side (hidden on mobile) */}
+      <div className="hidden md:flex shrink-0">
+        <Sidebar activeItem="jobs-near-you" />
+      </div>
 
       {/* Map Component - Right Side (Main Content) */}
       <Map />
